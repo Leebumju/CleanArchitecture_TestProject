fadca9d109b8c59abd657bda68b323dd48011fa9
jun12 <jun12@utransfer.com>
2025-08-26 13:38:16 +0900

    first commit

---
+++ Remember_TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remember_TestProject
+//
+//  Created by 이범준 on 8/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
